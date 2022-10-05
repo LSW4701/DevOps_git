f676b11ba314bd347ac3344479979938f40788cb
lsw4701 <pro_4701@naver.com>
2022-10-05 23:14:18 +0900

    1121

--- 3-docker-kubernetes/env/terraform-eks/3-irsa/irsa.tf
+++ 3-docker-kubernetes/env/terraform-eks/3-irsa/irsa.tf
@@ -1,3 +1,4 @@
+# 3개의 irsa 
 module "irsa__irsa_test" {
   source  = "tedilabs/container/aws//modules/eks-irsa"
   version = "0.14.0"
@@ -5,8 +6,8 @@ module "irsa__irsa_test" {
   name        = "irsa-${module.cluster.name}-app-irsa-test"
   description = "EKS IAM Role for Service Account of irsa-test app."
 
-  oidc_provider_urls       = [module.cluster.oidc_provider_urn]
-  trusted_service_accounts = ["fastcampus:irsa-test"]
+  oidc_provider_urls       = [module.cluster.oidc_provider_urn]  # 클러스터 생성시 주어지는 oidc 값을 연결 
+  trusted_service_accounts = ["fastcampus:irsa-test"]   # 클러스터 내에서 fastcampus 네임스페이스의 irsa-test pod가  해당 IAM role을 사용한다.
 
   inline_policies = {
     "this" = file("policies/irsa-test.json")
--- 3-docker-kubernetes/env/terraform-eks/3-irsa/node-groups.tf
+++ 3-docker-kubernetes/env/terraform-eks/3-irsa/node-groups.tf
@@ -19,13 +19,13 @@ module "node_group__app" {
   cluster_name = module.cluster.name
   name         = "${module.cluster.name}-app-v1.21"
 
-  desired_size = 2
+  desired_size = 2  # autuscale 
   min_size     = 1
   max_size     = 2
 
   instance_type    = "t3.small"
-  instance_ami     = "ami-0073aeb06ceb4b0dc"
-  instance_ssh_key = "fastcampus"
+  instance_ami     = "ami-0073aeb06ceb4b0dc"    # ami 최신 버전 매번 바뀌니 하드코딩 
+  instance_ssh_key = "linux1" # 개인 public ssh 키
   instance_profile = module.cluster.iam_roles["node"].instance_profile_name
 
   associate_public_ip_address = true
