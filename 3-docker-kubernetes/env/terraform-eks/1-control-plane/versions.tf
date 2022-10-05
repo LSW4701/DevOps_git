dc8fccbe940d426847b883cea55922cb01bf08a2
lsw4701 <pro_4701@naver.com>
2022-10-05 21:05:00 +0900

    21

--- 3-docker-kubernetes/env/terraform-eks/1-control-plane/versions.tf
+++ 3-docker-kubernetes/env/terraform-eks/1-control-plane/versions.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = "~> 1.3.0"  # 테라폼에 대한 버전 명시 required_version = "~> 1.0.0" 
+  required_version = "~> 1.3.0"  # error) Unsupported Terraform Core version 에러로 버전 수정 required_version = "~> 1.0.0" 
 
   required_providers {
     aws = {
